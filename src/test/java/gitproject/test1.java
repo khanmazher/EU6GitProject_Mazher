8e688467c59422e95dc02cb6b99bc3680beb6824
khanmazher <khanmazher@gmail.com>
2021-09-11 22:07:58 +0300

    My second commit

--- src/test/java/gitproject/test1.java
+++ src/test/java/gitproject/test1.java
@@ -3,5 +3,7 @@ package gitproject;
 public class test1 {
     public static void main(String[] args) {
         System.out.println("My first project with GIT");
+
+        System.out.println("I made some more changes to my project");
     }
 }
