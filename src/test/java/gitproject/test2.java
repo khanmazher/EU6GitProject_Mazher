c7a19b010c89ca1f0cb8d22a4ffe198481ce0c0b
khanmazher <khanmazher@gmail.com>
2021-09-11 22:44:44 +0300

    My second commit to FeatureB branch

--- src/test/java/gitproject/test2.java
+++ src/test/java/gitproject/test2.java
@@ -3,5 +3,6 @@ package gitproject;
 public class test2 {
     public static void main(String[] args) {
         System.out.println("My second file");
+        System.out.println("I am adding more features");
     }
 }
