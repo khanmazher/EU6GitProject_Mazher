67793ba179f7b4b4e60b47a2690a93bd390b2183
khanmazher <khanmazher@gmail.com>
2021-09-11 22:35:54 +0300

    My second commit to FeatureA Branch

--- src/test/java/gitproject/FeatureATest1.java
+++ src/test/java/gitproject/FeatureATest1.java
@@ -3,5 +3,7 @@ package gitproject;
 public class FeatureATest1 {
     public static void main(String[] args) {
         System.out.println("I made some awesome work on my featureA");
+
+        System.out.println("I added some more work");
     }
 }
